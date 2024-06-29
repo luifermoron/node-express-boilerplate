d790aeccfa5d7cf5d541a605a95186b24958bc7b
Luis Fernando Moron <contact@luifermoron.com>
2024-06-29 15:57:37 -0400

    feature: add helment for security

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express, { Request, Response } from 'express';
-
+import express, { Request, Response, NextFunction } from 'express';
+import helmet from 'helmet';
 const app = express();
 const port = process.env.PORT || 3000;
 import routes from './drivers/express/routes';
 const { scopePerRequest, loadControllers } = require('awilix-express');
 
 import container from './drivers/dependecyinjection/container';
+
+
+app.use(helmet());
+
+app.disable('x-powered-by');
+
+// Remove or change the X-Powered-By header
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.removeHeader('X-Powered-By');
+  next();
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
